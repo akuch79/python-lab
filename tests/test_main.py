cbd78d422c4bbc9143616772bdd61a8872521c0a
maggy Akuch Deng <margaretakuch2019@gmail.com>
2026-08-21 13:58:33 +0300

    Add unit tests for utility functions

---
+++ tests/test_main.py
@@ -0,0 +1,15 @@
+from src.utils import square, is_even, celsius_to_fahrenheit
+
+
+def test_square():
+    assert square(5) == 25
+
+
+def test_is_even():
+    assert is_even(4) is True
+    assert is_even(5) is False
+
+
+def test_celsius_to_fahrenheit():
+    assert celsius_to_fahrenheit(0) == 32
+    assert celsius_to_fahrenheit(100) == 212
